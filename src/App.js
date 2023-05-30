ab0461f6379d8adf119998331d5828fda5aae54d
tpandey495 <tpandey495@gmail.com>
2023-05-30 08:18:51 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header';
+import Ui from './component/Ui';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Ui />
     </div>
   );
 }
---
+++ src/component/Ui.js
@@ -0,0 +1,4 @@
+const Ui=()=><div className="ui">UI/UX  {`>`}Refer & Earn </div>;
+
+
+export default Ui;
